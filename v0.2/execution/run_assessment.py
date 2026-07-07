acb7d22648d03503abc1eb603faf1394b2f37c55
Sillinous <travisgray85@gmail.com>
2026-07-06 19:34:18 -0500

    Connect GPS v0.2 assessment runner to model conversion flow

--- v0.2/execution/run_assessment.py
+++ v0.2/execution/run_assessment.py
@@ -1,13 +1,18 @@
 class AssessmentRunner:
     """Coordinate GPS assessment execution."""
 
-    def __init__(self, loader, repository, engine, reporter):
+    def __init__(self, loader, converter, repository, engine, reporter):
         self.loader = loader
+        self.converter = converter
         self.repository = repository
         self.engine = engine
         self.reporter = reporter
 
     def run(self, model_path):
-        model = self.loader.load(model_path)
+        raw_model = self.loader.load(model_path)
+        model = self.converter.convert(raw_model)
+
+        self.repository.save(model)
+
         findings = self.engine.evaluate(model)
         return self.reporter.generate(findings)
